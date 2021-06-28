7965bf634b9f47c84cc3f2451141724cbdffed4d
Ailton Salguero <ailton.salg@gmail.com>
2021-06-27 20:43:34 -0500

    click card

--- arlocation.js
+++ arlocation.js
@@ -1,6 +1,6 @@
 AFRAME.registerComponent("foo", {
     init: function () {
-        this.el.addEventListener("click", function (e) {
+        this.el.addEventListener("click", () => {
             console.log(e);
             console.log("click");
             window.location.href = "./store_detail.html";

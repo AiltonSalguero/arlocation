6c195c7b7c498580cdbcbd1e6567671000b07a94
Ailton Salguero <ailton.salg@gmail.com>
2021-06-27 21:09:21 -0500

    click card touch 2

--- arlocation.js
+++ arlocation.js
@@ -1,14 +1,13 @@
+function to_store_detail(evt) {
+    evt.preventDefault()
+    console.log(evt);
+    console.log("click");
+    window.location.href = "./store_detail.html";
+}
 AFRAME.registerComponent("foo", {
     init: function () {
-        this.el.addEventListener("click", () => {
-            console.log(e);
-            console.log("click");
-            window.location.href = "./store_detail.html";
-        })
-        this.el.addEventListener("touchstart", () => {
-            console.log(e);
-            console.log("click");
-            window.location.href = "./store_detail.html";
-        });
+        console.log(this);
+        this.el.addEventListener('touchstart', to_store_detail)
+        this.el.addEventListener('click', to_store_detail)
     }
 })

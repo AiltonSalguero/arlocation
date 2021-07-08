8a5a5c4e9b7bdff9ca79e506d360ef793bc49e9b
Ailton Salguero <ailton.salg@gmail.com>
2021-07-07 19:55:23 -0500

    card bigger

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function get_data_stores() {
     const url = 'https://gamarraappserviceapi.azurewebsites.net/api/Usuario/ListUsuariosByCoordenadas/200/200';
     fetch(url).then(res => res.json()).then(data => {
         const stores_data = data['usuariosRolTiendaByQuery']
-        console.log(stores);
+        console.log(stores_data);
         localStorage.setItem('stores_data', JSON.stringify(stores_data));
 
     })

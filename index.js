ed9f9693497cbb95194a916638f75ae109d60fb0
Ailton Salguero <ailton.salg@gmail.com>
2021-07-07 20:34:58 -0500

    fixed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function get_data_stores() {
     const url = 'https://gamarraappserviceapi.azurewebsites.net/api/Usuario/ListUsuariosByCoordenadas/200/200';
     fetch(url).then(res => res.json()).then(data => {
         const stores_data = data['usuariosRolTiendaByQuery']
+        console.log(stores_data);
         stores_data.forEach(store => {
             store.direccion_Latitud = parseFloat(store.direccion_Latitud)
             store.direccion_Longitud = parseFloat(store.direccion_Longitud)
